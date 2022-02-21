3b2b7058234ae9c370d56cb643796decc3cf5bd9
AlaricStonewood <patriciob@greymountain.info>
2022-02-20 22:53:53 -0600

    First Commit

---
+++ main_img.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+a = Image.open('maxresdefault.jpg')
+
+img = a.resize((4096, 2042), Image.ANTIALIAS)
+
+img.show()
+
+fig, axs = plt.subplots(2, 2, figsize=(5, 6), constrained_layout=True)
+axs[0, 0].imshow(a, interpolation='nearest', cmap='RdBu_r')
+axs[0, 0].set_xlim(100, 200)
+axs[0, 0].set_ylim(275, 175)
+axs[0, 0].set_title('Zoom')
+
+for ax, interp, space in zip(axs.flat[1:],
+                             ['nearest', 'antialiased', 'antialiased'],
+                             ['data', 'data', 'rgba']):
+    ax.imshow(a, interpolation=interp, interpolation_stage=space,
+              cmap='RdBu_r')
+    ax.set_title(f"interpolation='{interp}'\nspace='{space}'")
+plt.show()
+
+fig, ax = plt.subplots(figsize=(6.8, 6.8))
+ax.imshow(a, interpolation='nearest', cmap='gray')
+ax.set_title("upsampled by factor a 1.048, interpolation='nearest'")
+plt.show()
+
+fig, ax = plt.subplots(figsize=(6.8, 6.8))
+ax.imshow(a, interpolation='antialiased', cmap='gray')
+ax.set_title("upsampled by factor a 1.048, interpolation='antialiased'")
+plt.show()
+
+fig, axs = plt.subplots(1, 2, figsize=(7, 4), constrained_layout=True)
+for ax, interp in zip(axs, ['hanning', 'lanczos']):
+    ax.imshow(a, interpolation=interp, cmap='gray')
+    ax.set_title(f"interpolation='{interp}'")
+plt.show()
